19e99c4e5f3cc97a2d9c8ec9105f53173ee85bba
Emanuel Bennici <benniciemanuel78@gmail.com>
2019-08-02 00:14:27 +0200

    //WIP Add configure SubCommand Go File
    THe 'configure' Command is used the create the Configured needed by this
    CLI
    
    Signed-off-by: Emanuel Bennici <benniciemanuel78@gmail.com>

---
+++ cmd/configure.go
@@ -0,0 +1,83 @@
+/*
+Copyright © 2019 Emanuel Bennici <benniciemanuel78@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+// configureCmd represents the configure command
+var configureCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "Create and Setup Configuration File",
+	Long: `configure creates a Config File under
+ HOME/.oima.yaml and configures it with your Input.`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		err := configure()
+
+		if err != nil {
+			_ = fmt.Errorf("error while creating Configuration: %s", err.Error())
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configureCmd)
+
+	// TODO: Add Flags to make non-interactive configuration Possible
+}
+
+func configure() error {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println(">>>>> Docker Registry Configuration <<<<<")
+	fmt.Print("Enter Docker Registry URI: ")
+	registryURI, _ := reader.ReadString('\n')
+	viper.Set("registry_uri", registryURI)
+
+	fmt.Print("Is Authentication Required? [Y|n]: ")
+	authReq, _ := reader.ReadString('\n')
+
+	if len(authReq) == 0 || strings.ToLower(authReq) == "y" {
+		fmt.Print("Enter Username: ")
+		dockerUsername, _ := reader.ReadString('\n')
+		dockerPasswd, _ := terminal.ReadPassword(int(syscall.Stdin))
+
+		if len(dockerPasswd) == 0 || len(dockerUsername) == 0 {
+			fmt.Println("Authentication is required. Please enter a Valid Username or Password!!")
+			os.Exit(1)
+		}
+
+		viper.Set("registry_username", dockerUsername)
+		viper.Set("registry_password", dockerPasswd)
+	}
+}
