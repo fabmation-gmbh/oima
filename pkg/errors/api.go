c74181aa4b2c8f2b78bacea1f14c6ef63829ec65
Emanuel Bennici <benniciemanuel78@gmail.com>
2019-08-03 23:46:16 +0200

    Add Error ImageNameNotDefined
    Signed-off-by: Emanuel Bennici <benniciemanuel78@gmail.com>

--- pkg/errors/api.go
+++ pkg/errors/api.go
@@ -21,3 +21,25 @@ func NewRepositoryNameNotDefinedErrorMsg(message string) *RepositoryNameNotDefin
 func (e *RepositoryNameNotDefinedError) Error() string {
 	return e.message
 }
+
+/// -=-=-=-=-=-=-=-=-=] ImageNameNotDefinedError [-=-=-=-=-=-=-=-=-=
+
+// ImageNameNotDefinedError occurs when the Application tries to get Information
+// about a Repository but the Repository Name is not defined/ set.
+type ImageNameNotDefinedError struct { message string }
+
+func NewImageNameNotDefinedError() *ImageNameNotDefinedError {
+	return &ImageNameNotDefinedError{
+		message: "Internal Error: Image Name not defined",
+	}
+}
+
+func NewImageNameNotDefinedErrorMsg(message string) *ImageNameNotDefinedError {
+	return &ImageNameNotDefinedError{
+		message: message,
+	}
+}
+
+func (e *ImageNameNotDefinedError) Error() string {
+	return e.message
+}
