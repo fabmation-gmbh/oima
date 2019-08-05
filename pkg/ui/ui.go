bd5c99b4785bc707234bed16b2afd8409df5f328
Emanuel Bennici <eb@fabmation.de>
2019-08-05 14:13:40 +0200

    Add ui Package
    In Package _ui_ is the Code which is responsible to show the UI at the
    Terminal.

---
+++ pkg/ui/ui.go
@@ -0,0 +1,200 @@
+package ui
+
+import (
+	"github.com/awnumar/memguard"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+
+	. "github.com/fabmation-gmbh/oima/internal/log"
+	"github.com/fabmation-gmbh/oima/pkg/registry"
+)
+
+var dockerReg registry.DockerRegistry
+var tree []*widgets.TreeNode
+var grid *ui.Grid
+var repoImageTree *widgets.Tree
+var stats *widgets.List
+
+
+type nodeValue string
+
+func (nv nodeValue) String() string { return string(nv) }
+
+func StartUI() {
+	initRegistry()
+
+	if err := ui.Init(); err != nil {
+		Log.Fatalf("failed to initialize termui: %v", err);
+		memguard.SafeExit(1)
+	}
+	defer ui.Close()
+
+	// initialize Grid
+	initGrid()
+
+	// data
+	tree = getTree()
+
+	// widgets
+	repoImageTree = widgets.NewTree()
+
+	// draw UI
+	drawFunction()
+
+	previousKey := ""
+	uiEvents := ui.PollEvents()
+	for {
+		e := <-uiEvents
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		case "j", "<Down>":
+			repoImageTree.ScrollDown()
+		case "k", "<Up>":
+			repoImageTree.ScrollUp()
+		case "<C-d>":
+			repoImageTree.ScrollHalfPageDown()
+		case "<C-u>":
+			repoImageTree.ScrollHalfPageUp()
+		case "<C-f>":
+			repoImageTree.ScrollPageDown()
+		case "<C-b>":
+			repoImageTree.ScrollPageUp()
+		case "g":
+			if previousKey == "g" { repoImageTree.ScrollTop() }
+		case "<Home>":
+			repoImageTree.ScrollTop()
+		case "<Enter>", "<Space>":
+			repoImageTree.ToggleExpand()
+		case "G", "<End>":
+			repoImageTree.ScrollBottom()
+		case "E":
+			repoImageTree.ExpandAll()
+		case "C":
+			repoImageTree.CollapseAll()
+		case "<Resize>":
+			x, y := ui.TerminalDimensions()
+			repoImageTree.SetRect(0, 0, x, y)
+		}
+
+		if previousKey == "g" {
+			previousKey = ""
+		} else {
+			previousKey = e.ID
+		}
+
+		// re-render UI
+		ui.Render(grid)
+	}
+}
+
+func drawFunction() {
+	// TODO: add small Stats-Panel at the right side
+	// TODO: move me to own Function
+	stats = widgets.NewList()
+	stats.Title = "Registry Stats"
+	stats.TextStyle = ui.NewStyle(ui.ColorYellow)
+	stats.WrapText = false
+	stats.SetRect(0, 0, 25, 8)
+
+	repoImageTree.TextStyle = ui.NewStyle(ui.ColorYellow)
+	repoImageTree.WrapText = false
+	repoImageTree.SetNodes(tree)
+
+	x, y := ui.TerminalDimensions()
+
+	repoImageTree.SetRect(0, 0, x, y)
+
+	// add Items to grid
+	grid.Set(
+		ui.NewRow(1,
+			ui.NewCol(0.75, repoImageTree), // Repo/ Image Tree	(75%)
+			ui.NewCol(0.25, stats),         // Stats Panel		(25%)
+		),
+	)
+
+	// render and show the UI
+	ui.Render(grid)
+}
+
+func getTree() []*widgets.TreeNode {
+	// get List of Repositories
+	repos := dockerReg.ListRepositories()
+	Log.Debugf("Registry %s has %d Repositories", dockerReg.URI, len(repos))
+
+	// create Tree
+	var nodes []*widgets.TreeNode
+	var _nodes []*widgets.TreeNode
+
+	for _, v := range repos {
+		images, err := v.ListImages()
+		if err != nil {
+			Log.Criticalf("Error while getting List of Images in Repo %s: %s", v.Name, err.Error())
+			memguard.SafeExit(1)
+		}
+
+		var imageEntries []*widgets.TreeNode
+
+		// fill 'treeEntry' with Images
+		for _, img := range images {
+			imgEntry := widgets.TreeNode{
+				Value:    nodeValue(img.Name),
+				Expanded: false,
+				Nodes:    nil, // TODO: Implement Digest Information
+			}
+
+			imageEntries = append(imageEntries, &imgEntry)
+		}
+
+		// append Images to Repo Entry
+		repoEntry := widgets.TreeNode{
+			Value:    nodeValue(v.Name),
+			Expanded: false,
+			Nodes:    imageEntries,
+		}
+
+		// add Repo Entry to Registry Entry-Node
+		_nodes = append(_nodes, &repoEntry)
+	}
+
+	nodes = []*widgets.TreeNode{
+		{
+			Value:    nodeValue(dockerReg.URI),
+			Expanded: true,
+			Nodes:    _nodes,
+		},
+	}
+
+	return nodes
+}
+
+func initGrid() {
+	// create grid
+	grid = ui.NewGrid()
+	termWidth, termHeight := ui.TerminalDimensions()
+	grid.SetRect(0, 0, termWidth, termHeight)
+}
+
+/// >>>>> internal Functions <<<<<
+
+// Initialize Docker Registry and handle Errors
+// TODO: better handle custom Errors!
+func initRegistry() {
+	err := dockerReg.Init()
+	if err != nil {
+		Log.Panicf("Error while Initialize DockerRegistry: %s", err.Error())
+	}
+
+	// Fetch All Informations from Docker Registry
+	registryFetch()
+}
+
+// Fetch all Informations form the Docker Registry and handle Errors
+// TODO: better handle custom Errors!
+func registryFetch() {
+	err := dockerReg.FetchAll()
+	if err != nil {
+		Log.Fatalf("Error while Fetching All Informations from Registry '%s': %s", dockerReg.URI, err.Error())
+		memguard.SafeExit(1)
+	}
+}
