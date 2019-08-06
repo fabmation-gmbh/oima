1b5c9b5307d5803f2a576e300dfc84a847cc4031
Emanuel Bennici <eb@fabmation.de>
2019-08-06 15:24:15 +0200

    Add new Struct 'Stats' to pkg/registy/interfaces
    _Stats_ holds Informations/ Statistics of a Registry

--- pkg/registry/interfaces/base_types.go
+++ pkg/registry/interfaces/base_types.go
@@ -1,3 +1,19 @@
 package interfaces
 
 type TagName string             // TagName of an Image (for example 'v1.0.0' or '0.1.0-beta')
+
+
+// Stats contains statistics of a Registry
+type Stats struct {
+	// Number of Repositories
+	Repos				int
+
+	// Number of Images
+	Images				int
+
+	// Number of Tags
+	Tags				int
+
+	// Number of Signatures found on the S3 Server
+	S3Signatures		int
+}
